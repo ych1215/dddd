ba2781228c771b365ea85ed4847f4fd8e9fe5ed8
ych1215 <53690231+ych1215@users.noreply.github.com>
2023-07-19 23:00:08 +0900

    Update HomeController.java
    Signed-off-by: ych1215 <53690231+ych1215@users.noreply.github.com>

--- css/src/main/java/com/aaa/bbb/HomeController.java
+++ css/src/main/java/com/aaa/bbb/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Handles requests for the application home page.
+ * Handles requests for the application home page.  테스트당.......
  */
 @Controller
 public class HomeController {
