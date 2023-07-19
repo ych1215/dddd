30a5e092fe7d57532cf1e0dc7b7d38e44537363e
ych1215 <ych1215@DESKTOP-8K0STBB>
2023-07-19 23:14:38 +0900

    ㅁㅁㅁㅁ

--- css/src/main/java/com/aaa/bbb/HomeController.java
+++ css/src/main/java/com/aaa/bbb/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Handles requests for the application home page.  테스트당.......99999999999999999999999
+ * Handles requests for the application home page. 
  */
 @Controller
 public class HomeController {
